22ef86050b9a561c58332b3b4d4f1cc2503a7094
Tassilo <tassia710@gmail.com>
2020-05-18 01:14:25 +0200

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -54,3 +54,36 @@ function isEmail(str) {
 	return re.test(str);
 }
 /* Utility */
+
+
+
+
+
+/* Cookies */
+function setCookie(name, value, domain) {
+	var d = new Date();
+	d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+	var expires = 'expires=' + d.toUTCString();
+	if (domain) document.cookie = name + '=' + value + ';' + expires + ';path=/;domain=' + domain;
+	else document.cookie = name + '=' + value + ';' + expires + ';path=/';
+}
+
+function getCookie(name) {
+	var name = name + '=';
+	var ca = document.cookie.split(';');
+	for(var i = 0; i < ca.length; i++) {
+	  var c = ca[i];
+	  while (c.charAt(0) == ' ') {
+	    c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+	    return c.substring(name.length, c.length);
+	  }
+	}
+	return '';
+}
+
+function unsetCookie(name) {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+}
+/* Cookies */
