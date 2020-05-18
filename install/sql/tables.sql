7ceaac09aaa2896a276bb51b2c3b9c6728374d97
Tassilo <tassia710@gmail.com>
2020-05-18 17:24:10 +0200

    Added default groupID

--- install/sql/tables.sql
+++ install/sql/tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `moodclap_accounts` (
 	`AccountID` BIGINT AUTO_INCREMENT,
 	`Username` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin,
 	`Password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin,
-	`GroupID` BIGINT,
+	`GroupID` BIGINT DEFAULT 1,
 	PRIMARY KEY (`AccountID`), UNIQUE (`Username`), INDEX (`GroupID`)
 );
 
