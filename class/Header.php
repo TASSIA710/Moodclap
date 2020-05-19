fd9d1cd10ae77ba85c4e532a724832613873af61
Tassilo <tassia710@gmail.com>
2020-05-19 18:13:47 +0200

    Added more functions to Header class

--- class/Header.php
+++ class/Header.php
@@ -3,6 +3,7 @@
 class Header {
 	private static $stylesheets = [];
 	private static $scripts = [];
+	private static $title = '';
 
 	/* Stylesheets */
 	public static function addStylesheet($stylesheet) {
@@ -20,6 +21,8 @@ class Header {
 
 
 
+
+
 	/* Scripts */
 	public static function addScript($script) {
 		self::$scripts[$script] = true;
@@ -33,5 +36,53 @@ class Header {
 		return self::$scripts;
 	}
 	/* Scripts */
+
+
+
+
+
+	/* Window Title */
+	public static function getTitle() {
+		return self::$title;
+	}
+
+	public static function setTitle($title) {
+		self::$title = $title;
+	}
+	/* Window Title */
+
+
+
+
+
+	/* Bootstrap */
+	public static function requireBootstrap() {
+		self::addStylesheet('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+		self::addScript('https://code.jquery.com/jquery-3.4.1.slim.min.js');
+		self::addScript('https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+		self::addScript('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+	}
+
+	public static function unrequireBootstrap() {
+		self::removeStylesheet('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+		self::removeScript('https://code.jquery.com/jquery-3.4.1.slim.min.js');
+		self::removeScript('https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+		self::removeScript('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+	}
+	/* Bootstrap */
+
+
+
+
+
+	/* Font Awesome */
+	public static function requireFontAwesome() {
+		self::addStylesheet('https://use.fontawesome.com/releases/v5.12.1/css/all.css');
+	}
+
+	public static function unrequireFontAwesome() {
+		self::removeStylesheet('https://use.fontawesome.com/releases/v5.12.1/css/all.css');
+	}
+	/* Font Awesome */
 
 }
