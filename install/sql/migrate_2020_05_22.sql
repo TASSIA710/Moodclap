789f66d534a0ab9f02633be52f854ae1715737e1
Tassilo <tassia710@gmail.com>
2020-05-22 22:53:38 +0200

    Reworked install TSQL files

--- install/sql/migrate_2020_05_22.sql
+++ install/sql/migrate_2020_05_22.sql
@@ -34,3 +34,34 @@ CREATE TABLE IF NOT EXISTS `moodclap_sessions` (
 	`LastIP` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin,
 	PRIMARY KEY (`Token`), INDEX (`AccountID`)
 );
+
+
+
+-- Truncate tables, to avoid collisions with the auto increment primary key function.
+TRUNCATE `moodclap_groups`;
+
+
+
+-- Insert 'Member' group
+INSERT INTO `moodclap_groups` (GroupID, GroupNameID, GroupName, Description, Permissions, SortDisplay, SortPermission) VALUES (
+	1,
+	'member',
+	'Member',
+	'The default group for new accounts.',
+	'{}',
+	99,
+	99
+);
+
+
+
+-- Insert 'System Administrator' group
+INSERT INTO `moodclap_groups` (GroupID, GroupNameID, GroupName, Description, Permissions, SortDisplay, SortPermission) VALUES (
+	2,
+	'sysadmin',
+	'System Administrator',
+	'The highest available group for the root account.',
+	'{"*":true}',
+	1,
+	0
+);
