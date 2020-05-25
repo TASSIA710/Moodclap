804d29489eced093822e873d534e634091bc6ced
Tassilo <tassia710@gmail.com>
2020-05-25 14:21:07 +0200

    Added Markdown class

---
+++ class/Markdown.php
@@ -0,0 +1,15 @@
+<?php
+
+class Markdown {
+
+	/* Parse */
+	public static function parse($source, $safeMode = true, $escapeHTML = true) {
+		// TODO
+	}
+
+	public static function parseInline($source, $safeMode = true, $escapeHTML = true) {
+		// TODO
+	}
+	/* Parse */
+
+}
